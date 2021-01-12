e9729f966a409239f7699c8b8210eed650bfb119
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-01-12 16:18:44 +0100

    Add Ascii-art basic prog

---
+++ ascii/filetolines.go
@@ -0,0 +1,29 @@
+package ascii
+
+import (
+	"bufio"
+	"os"
+)
+
+//lecture du fichier ascii.txt dans lequel se trouve tous les caractères en ASCII-Art
+//Puis mise de chaque ligne dans un tableau de string
+
+func FilesToLines(filePath string) (asciiArtLines []string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil
+	}
+	defer file.Close() //fermer le fichier au moment du "return"
+
+	scanner := bufio.NewScanner(file) //
+	for scanner.Scan() {
+		asciiArtLines = append(asciiArtLines, scanner.Text())
+	}
+	//attention ici il faut vérifier si c'est bon (car la c'est pas fait)
+	err = scanner.Err()
+	if err != nil {
+		return nil
+	}
+	return asciiArtLines
+
+}
