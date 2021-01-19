d5d00f097e5a5874ac731ec98c9c2018c934e8c4
Mathéo LEGER <matheo.leger@ynov.com>
2021-01-19 10:47:36 +0100

    Update for Error 404 (and path)
    - define /ascii-art path
    - create an management of Error 404
    - update printascii.go (9 lines instead of 8)

--- ascii/server/server.go
+++ ascii/server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 
@@ -15,12 +16,27 @@ func server(w http.ResponseWriter, req *http.Request) {
 		panic(err)
 	}
 
+	if req.URL.Path != "/" && req.URL.Path != "/ascii-art" {
+		errorHandler(w, req, http.StatusNotFound)
+		return
+	}
+
 	formSelect := req.Form.Get("select")
 	formText := req.Form.Get("text")
 
 	finalStr := asciifunc.Ascii(formText, formSelect)
 
+	//fmt.Fprintf(w, "Test Fprintf")
+
 	t.Execute(w, template.HTML(finalStr))
+
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "error 404")
+	}
 }
 
 func main() {
@@ -28,7 +44,7 @@ func main() {
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static")))) // récupère tous les fichiers "externe" (comme le style.css)
 
-	http.HandleFunc("/", server) // "/" pour dire qu'on est dans ce fichier (je crois) et server car cest la fonction
+	http.HandleFunc("/", server) // "/" pour dire qu'on est dans ce chemin et server car cest la fonction
 
 	http.ListenAndServe(":50000", nil)
 }
